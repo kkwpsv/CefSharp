0a654eb39808fdb369e312b3d3b5ba02aa81249a
Shaojun Li <i@shaojun.li>
2019-12-26 11:51:05 +0800

    Use discrete graphics card

--- CefSharp.Wpf.D3D11Renderer/pch.cpp
+++ CefSharp.Wpf.D3D11Renderer/pch.cpp
@@ -1,5 +1,10 @@
-﻿// pch.cpp: 与预编译标头对应的源文件
+// pch.cpp: 与预编译标头对应的源文件
 
 #include "pch.h"
 
 // 当使用预编译的头时，需要使用此源文件，编译才能成功。
+extern "C"
+{
+    __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+    __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
